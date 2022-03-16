fa9afe52b22880121f68a9eb74a508034292d1ba
Juan Medina <medina189153@gmail.com>
2022-03-16 16:33:26 -0300

    hot fix: change name of an environment variable

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT,
-  dbUrl: process.env.DB_URL,
+  dbUrl: process.env.DATABASE_URL,
   secret: process.env.secret,
   isProduction: process.env.NODE_ENV === 'production'
 }
